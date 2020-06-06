e4fa01611db4c3655e2d22322beff2f032afa753
liukan <liukan1012@163.com>
2020-06-06 09:22:28 +0800

    Better README.md and Makefile

--- Tridiagonal_CUDA/tridiagonal_third_party.cu
+++ Tridiagonal_CUDA/tridiagonal_third_party.cu
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cusparse.h>
 #include "alg/lib-BPLG/KTridiagCR.hxx"
 #include "alg/lib-BPLG/KTridiagPCR.hxx"
 #include "alg/lib-BPLG/KTridiagWM.hxx"
@@ -15,50 +14,6 @@
     }                                                                 \
 }
 
-static cusparseHandle_t cusparse_handle;
-static void *buffer;
-
-int cuSparse_init_single(float *a_gbl, float *b_gbl, float *c_gbl, float *d_gbl, int n_eqt, int n_batch) {
-    size_t size;
-    int ret = cusparseCreate(&cusparse_handle);
-    if(ret != CUSPARSE_STATUS_SUCCESS) {
-        return ret;
-    }
-    cusparseSgtsv2StridedBatch_bufferSizeExt(cusparse_handle, n_eqt, a_gbl, b_gbl, c_gbl, d_gbl, n_batch, n_eqt, &size);
-    ret = cudaMalloc(&buffer, size);
-    if(ret != cudaSuccess) {
-        cusparseDestroy(cusparse_handle);
-    }
-    return ret;
-}
-
-int cuSparse_init_double(double *a_gbl, double *b_gbl, double *c_gbl, double *d_gbl, int n_eqt, int n_batch) {
-    size_t size;
-    int ret = cusparseCreate(&cusparse_handle);
-    if(ret != CUSPARSE_STATUS_SUCCESS) {
-        return ret;
-    }
-    cusparseDgtsv2StridedBatch_bufferSizeExt(cusparse_handle, n_eqt, a_gbl, b_gbl, c_gbl, d_gbl, n_batch, n_eqt, &size);
-    ret = cudaMalloc(&buffer, size);
-    if(ret != cudaSuccess) {
-        cusparseDestroy(cusparse_handle);
-    }
-    return ret;
-}
-
-void cuSparse_final() {
-    cudaFree(buffer);
-    cusparseDestroy(cusparse_handle);
-}
-
-void cuSparse_single(float *a_gbl, float *b_gbl, float *c_gbl, float *d_gbl, int n_eqt, int n_batch) {
-    cusparseSgtsv2StridedBatch(cusparse_handle, n_eqt, a_gbl, b_gbl, c_gbl, d_gbl, n_batch, n_eqt, buffer);
-    cudaDeviceSynchronize();
-}
-void cuSparse_double(double *a_gbl, double *b_gbl, double *c_gbl, double *d_gbl, int n_eqt, int n_batch) {
-    cusparseDgtsv2StridedBatch(cusparse_handle, n_eqt, a_gbl, b_gbl, c_gbl, d_gbl, n_batch, n_eqt, buffer);
-    cudaDeviceSynchronize();
-}
 
 void BPLG_WM(float *a_gbl, float *b_gbl, float *c_gbl, float *d_gbl, int n_eqt, int n_batch) {
     int ret = KTridiagWM(a_gbl, 0, n_eqt, 4, n_batch);
